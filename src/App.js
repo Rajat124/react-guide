14da69e4b290823ae1caeaafe2e7f1fa0ca318d9
Rajat Sundriyal <rajatsundriyal1@gmail.com>
2023-06-30 09:33:59 +0530

    Making Components Smaller and Readable

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import ExpenseDate from "./components/ExpenseDate";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
@@ -41,6 +42,30 @@ function App() {
           location={LocationOfExpenditure}
         />
       ))}
+      {/* <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+        location={LocationOfExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+        location={LocationOfExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+        location={LocationOfExpenditure}
+      ></ExpenseItem>
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+        location={LocationOfExpenditure}
+      ></ExpenseItem> */}
     </div>
   );
 }
